f7f65d8ddaec83d65e0c61dd867902a9e5026427
Jaime Leon <jaime.leon@globant.com>
2021-08-15 15:13:13 -0500

    APPGATE:For now we going to support only light mode and portrait orientation.

--- Appgate-LoginTests/Appgate_LoginTests_String_Helper.swift
+++ Appgate-LoginTests/Appgate_LoginTests_String_Helper.swift
@@ -9,24 +9,24 @@ import XCTest
 
 class Appgate_LoginTests_String_Helper: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testIsValidEmail(){
+        let validEmail = "email@gmail.com"
+        XCTAssertTrue(validEmail.isValidEmail)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testIsInvalidEmail(){
+        let invalidEmail = "noemailhere"
+        XCTAssertFalse(invalidEmail.isValidEmail)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testIsValidPassword(){
+        let validPassword = "JYlp123@#"
+        XCTAssertTrue(validPassword.isValidPassword)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testIsInvalidValidPassword(){
+        let invalidPassword = "admin123"
+        XCTAssertFalse(invalidPassword.isValidPassword)
     }
 
 }
