be10fa017aa20b0fd07cffda3ba378534ef4cb22
Jaime Leon <jaime.leon@globant.com>
2021-08-15 14:56:27 -0500

    APPGATE:Created tests for string extension.

---
+++ Appgate-LoginTests/Appgate_LoginTests_String_Helper.swift
@@ -0,0 +1,32 @@
+//
+//  Appgate_LoginTests_String_Helper.swift
+//  Appgate_LoginTests_String_Helper
+//
+//  Created by Jaime Yesid Leon Parada on 15/08/21.
+//
+
+import XCTest
+
+class Appgate_LoginTests_String_Helper: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
